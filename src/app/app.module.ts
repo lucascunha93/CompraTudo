82798aa4dc629ea1fd4a0e55ea5deb31511ec3ab
Lucas Cunha <you@example.com>
2018-09-04 18:48:07 -0300

    Criado componente footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import { ROUTES } from './app.routes';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './paginas/header/header.component';
 import { NavComponent } from './paginas/nav/nav.component';
+import { FooterComponent } from './paginas/footer/footer.component';
+import { HomeComponent } from './paginas/home/home.component';
 import { CarouselComponent } from './paginas/home/carousel/carousel.component';
 import { OfertasComponent } from './paginas/home/ofertas/ofertas.component';
 import { ContatoComponent } from './paginas/contato/contato/contato.component';
 import { ProdutosComponent } from './paginas/produtos/produtos.component';
 import { CarrinhoComponent } from './paginas/carrinho/carrinho/carrinho.component';
 import { SobreComponent } from './paginas/sobre/sobre/sobre.component';
-import { HomeComponent } from './paginas/home/home.component';
 import { ProdutoComponent } from './paginas/produtos/produto/produto.component';
 
 import { ProdutosService } from './services/produtos/produtos.service';
@@ -33,6 +34,7 @@ import { HttpClientModule } from '@angular/common/http';
     SobreComponent,
     HomeComponent,
     ProdutoComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
